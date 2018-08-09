9ac3f4915a497faf74cb764aa7539b9f7edf3feb
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-09 19:53:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime(number)
-  if number > 2 || number < 2
+  if number < 2
+    return false
+  end
+  
+  if number > 2 
   (2...number).each do |ele|
       if number % ele == 0 
         return false
